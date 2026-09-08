7329ab43466300cd809c0be279c7e5e279ca7902
Ayushee Yadav <144589455+ayushii89@users.noreply.github.com>
2026-09-09 01:59:59 +0530

    Solve Team

---
+++ AlgorithmXlr8-P005-Team/Solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+     int cnt = 0;
+ 
+    for(int i = 0; i < n; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+ 
+        
+        if((a == 1 && b == 1) || (a == 1 && c == 1) || (b == 1 && c == 1))
+        {
+            cnt++;
+        }
+    }
+    cout<<cnt;
+     return 0;
+
+}
